c967f28861a7c7be8bcebe2c187ef135876a1fbb
C272 <realrussianmanconfirmed@gmail.com>
2018-10-18 17:59:39 +0100

    Updating main branch with initial lexer parsing.

--- stork/Program.cs
+++ stork/Program.cs
@@ -10,7 +10,45 @@ namespace stork
     {
         static void Main(string[] args)
         {
-          
+            //Checking that the command line arguments are given.
+            if (args.Length<1)
+            {
+                Console.WriteLine("STKI E001: No command line arguments given, use --help for more information.");
+                return;
+            }
+
+            //Command line arguments are given, continue.
+            switch (args[0])
+            {
+                case "--help":
+                    //Printing the help section.
+                    return;
+            }
+
+            //Assuming parameter is a file, attempt to load.
+            string file = "";
+            if(!StorkIO.loadFile(args[0], ref file))
+            {
+                Console.WriteLine("STKI E002: Invalid file path given, or error reading the file.");
+                return;
+            }
+
+            //File is now loaded in, pass it to the lexer.
+            StorkLexer lexer = new stork.StorkLexer();
+            foreach (char c in file)
+            {
+                lexer.feed(c);
+            }
+
+            List<string> slist = lexer.getStringList();
+            List<Type> tlist = lexer.getTypeList();
+
+            Console.WriteLine(slist.Count);
+            for (int i=0; i<slist.Count; i++)
+            {
+                Console.WriteLine(slist[i]);
+                Console.WriteLine(tlist[i]);
+            }
         }
     }
 }
---
+++ stork/StorkLexer.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace stork
+{
+    //"Type" enumeration.
+    enum Type
+    {
+        identifier,
+        import_token,
+        _string,
+        _symbol,
+        number,
+        in_statement,
+        vartype,
+        nulltype
+    }
+
+    class StorkLexer
+    {
+        //Class constructor, currently empty, but may take parameters in the future.
+        public StorkLexer()
+        {
+
+        }
+
+        //"Feed" function, which lets the main program feed input into the lexer.
+        public void feed(char c)
+        {
+            //Checking if the statement is over. If not, add to current statement.
+            if (c==' '||c=='('||c=='{'||c==';')
+            {
+                Console.WriteLine("FIRED");
+                //Statement over, end statement and pass to the type interpreter.
+                interpretType(currentStatement);
+                //Reset currentStatement to blank.
+                currentStatement = "";
+            } else
+            {
+                currentStatement += c;
+            }
+        }
+
+        public void interpretType(string s)
+        {
+            Type typeOut;
+            //Switching to determine type.
+            switch (s)
+            {
+                case "void":
+                    typeOut = Type.nulltype;
+                    break;
+                case "int":
+                case "float":
+                case "bool":
+                case "string":
+                    typeOut = Type.vartype;
+                    break;
+                default:
+                    //Checking if it's a string.
+                    Regex stringReg = new Regex("^\\\"(\\w+)\\\"\\z");
+                    if (stringReg.IsMatch(s))
+                    {
+                        //Valid, type as string.
+                        typeOut = Type._string;
+                        s = s.Substring(1);
+                        s = s.Remove(s.Length - 1);
+                        
+                        //Send to the event list.
+                        lexerTypeList.Add(typeOut);
+                        lexerStringList.Add(s);
+                        return;
+                    }
+
+                    //Checking if it's an import command.
+                    Regex importReg = new Regex("^~import\\z");
+                    if (importReg.IsMatch(s))
+                    {
+                        //It's an import, type as such and push to event list.
+                        typeOut = Type.import_token;
+                        lexerTypeList.Add(typeOut);
+                        lexerStringList.Add(s);
+                    }
+
+                    //Not a string.
+                    break;
+            }
+        }
+
+        //Getters for the type and string list.
+        public List<Type> getTypeList()
+        {
+            return lexerTypeList;
+        }
+        public List<string> getStringList()
+        {
+            return lexerStringList;
+        }
+
+        //Private lexer properties.
+        private string currentStatement;
+        private List<Type> lexerTypeList = new List<Type>();
+        private List<string> lexerStringList = new List<string>();
+    }
+}
